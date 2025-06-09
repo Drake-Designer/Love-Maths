24cb7c0ce2964690b5feec6fd60e8e9efc2f6e08
Drake <dariocostarelli88@gmail.com>
2025-06-09 16:41:45 +0100

    JS: add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-console.log('Connected!');
+document.addEventListener('DOMContentLoaded', function () {
+  let buttons = document.getElementsByTagName('button');
+
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (this.getAttribute('data-type') === 'submit') {
+        alert('You clicked the submit button!');
+      } else {
+        let gameType = this.getAttribute('data-type');
+        alert(`You clicked a button of type: ${gameType}`);
+      }
+    });
+  }
+});
+// function runGame() {};
+
+// function checkAnswer() {};
+
+// function calculateCorrectAnswer() {};
+
+// function incrementScore() {};
+
+// function incrementWrongAnswer() {};
+
+// function displayAdditionQuestion() {};
+
+// function displaySubtractQuestion() {};
+
+// function displayMultiplyQuestion() {}
